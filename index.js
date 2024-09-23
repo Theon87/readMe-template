30ccc0c21148eeddb3b346819772ba8fc1b07888
theon87 <theonfrancis17@gmail.com>
2024-09-22 22:12:02 -0400

    adds array of questions for user input

--- index.js
+++ index.js
@@ -6,7 +6,49 @@ import fs from "fs";
 
 // DATA
 // TODO: Create an array of questions for user input
-
+const questions = [[
+    {
+        type: "input",
+        message: colors.magenta("What is the title of your repo"),
+        name: "title"
+    },
+    {
+        type: "input",
+        message: colors.magenta("What is the description of your repo?"),
+        name: "description"
+    },
+    {
+        type: "input",
+        message: colors.magenta("Describe the installation process."),
+        name: "installation"
+    },
+    {
+        type: "input",
+        message: colors.magenta("How should this app be used?"),
+        name: "usage"
+    },
+    {
+        type: "list",
+        message: colors.magenta("Which license did you use?"),
+        name: "license",
+        choices: ["MIT", "Creative Commons license family", "Microsoft Public License"]
+    },
+    {
+        type: "input",
+        message: colors.magenta("Describe how you want users to contribute."),
+        name: "contributing"
+    },
+    {
+        type: "input",
+        message: colors.magenta("Describe how to test."),
+        name: "tests"
+    },
+    {
+        type: "input",
+        message: colors.magenta("Where do you want messages to be sent?"),
+        name: "questions"
+    }
+]];
 
 // FUNCTIONS
 // TODO: Create a function to write README file
